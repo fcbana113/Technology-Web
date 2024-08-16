3d526a9b3cd14aee0cfafc31963f9a02e807e8af
fcbana113 <fcbana113@gmail.com>
2024-08-16 23:11:37 +0700

    init project

---
+++ WebBanDoCongNghe/Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using WebBanDoCongNghe.Models;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using System.Net;
+
+namespace WebBanDoCongNghe.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(MailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var message = new MailMessage();
+                    message.To.Add(new MailAddress("daungocanhquan@gmail.com"));
+                    message.From = new MailAddress(model.Email);
+                    message.Subject = model.Subject;
+                    message.Body = model.Message + "\n\n" + "Phone Number: " + model.PhoneNumber;
+                    message.IsBodyHtml = false;
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        var credential = new NetworkCredential
+                        {
+                            UserName = "daungocanhquan@gmail.com",
+                            Password = "hwiqfhcteybdxoxp"
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.EnableSsl = true;
+                        smtp.Send(message);
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error: " + ex.Message);
+                }
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ WebBanDoCongNghe/Models/MailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebBanDoCongNghe.Models
+{
+    public class MailModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
